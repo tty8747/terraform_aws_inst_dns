9cb30c9eba5250d45649ead727f3c7536c91c13a
Sergey Chulanov <tty8747@gmail.com>
2019-05-10 13:52:51 +0300

    terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "keypath" {
-  default = "~/.ssh/id_rsa.pub"
-	description = "path to pub key"
+  default     = "~/.ssh/id_rsa.pub"
+  description = "path to pub key"
 }
 
 variable "allowed_ports" {
-    description = "Allowed ports from/to host"
-    type        = "list"
-    default     = ["22", "80", "443"]
+  description = "Allowed ports from/to host"
+  type        = "list"
+  default     = ["22", "80", "443"]
 }
