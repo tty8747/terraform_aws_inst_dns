cb85ae8e3e4243c114e2ded7f0773f8c710e1f4f
Sergey Chulanov <tty8747@gmail.com>
2019-05-10 13:50:02 +0300

    Added security group output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,12 @@
 output "Instance public dns" {
-  value = "${aws_instance.vpn.public_dns}"
+  value       = "${aws_instance.vpn.public_dns}"
   description = "pub dns"
+}
+
+output "Security groups ID" {
+  value = "${aws_security_group.aws_sg.id}"
+}
+
+output "Security groups name" {
+  value = "${aws_security_group.aws_sg.name}"
 }
