c28df548a4f15abe5dcc283e5ea1ee4d906b3f3e
Sergey Chulanov <tty8747@gmail.com>
2019-08-01 09:33:09 +0300

    Replaced count to for_each. Terraform 0.12.6!

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@
 #  }
 #
 #  lifecycle {
-##   prevent_destroy = true
+#   prevent_destroy = true
 #    prevent_destroy = false
 #  }
 #
@@ -38,9 +38,13 @@ resource "aws_security_group" "aws_sg" {
 
 resource "aws_security_group_rule" "ingress_udp_ports" {
   type              = "ingress"
-  count             = length(var.allowed_udp_ports)
-  from_port         = element(var.allowed_udp_ports, count.index)
-  to_port           = element(var.allowed_udp_ports, count.index)
+# Comment for test for_each in terraform 0.12.6
+# count             = length(var.allowed_udp_ports)
+# from_port         = element(var.allowed_udp_ports, count.index)
+# to_port           = element(var.allowed_udp_ports, count.index)
+  for_each          = var.allowed_udp_ports
+  from_port         = each.value
+  to_port           = each.value
   protocol          = "udp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.aws_sg.id
